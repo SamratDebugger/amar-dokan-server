a6d620f3f16417a3974b2648b7dab60dac729c90
Samrat <md721023@gmail.com>
2026-04-27 02:09:44 +0600

    user user data get

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import client from "./database/mongodb.js";
 
 dotenv.config();
 const app = express();
@@ -13,10 +14,40 @@ app.use(
   }),
 );
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+// create user data
+const userCollection = client.db("amar-dokan").collection("users");
+
+app.post("/users", async (req, res) => {
+  const { username, email } = req.body;
+  const role = "customer";
+  const joined = new Date();
+  try {
+    await userCollection.insertOne({ username, email, role, joined });
+    res.status(200).send({ success: true });
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(400).send({ success: false, message: "User Already Exists" });
+    }
+    res.status(500).send({ success: false, message: "Failed To Create User" });
+  }
+});
+
+//get all user data
+app.get("/users/getAll", async (req, res) => {
+  try {
+    const customer = await userCollection.find({ role: "customer" }).toArray();
+    res.status(200).json(customer);
+  } catch (error) {
+    res.status(500).json({ message: "Can't get users" });
+  }
 });
